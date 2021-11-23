e2b6dcb4da6ee1108e102147273ad4ae2b713cb0
Hanjaewon0215 <cooljody@sookmyung.ac.kr>
2021-11-23 16:58:43 +0900

    sss

--- app/src/main/java/smu/example/cabling/MainActivity.java
+++ app/src/main/java/smu/example/cabling/MainActivity.java
@@ -2,13 +2,28 @@ package smu.example.cabling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
